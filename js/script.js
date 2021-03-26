d7997c947fa01a22e69bfeb2211c56a6b8978197
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-03-25 21:29:44 -0700

    single type pokemon -> array

--- js/script.js
+++ js/script.js
@@ -2,21 +2,21 @@ let pokemonList = [
   {
     name: 'Ivysaur',
     height: 1,
-    type: ['Grass', 'Poison'],
+    types: ['Grass', 'Poison'],
   },
   {
     name: 'Charmeleon',
     height: 1.1,
-    type: 'Fire',
+    types: ['Fire'],
   },
   {
     name: 'Ninetales',
     height: 1.1,
-    type: 'Fire',
+    types: ['Fire'],
   },
   {
     name: 'Tentacruel',
     height: 1.6,
-    type: ['Water', 'Poison'],
+    types: ['Water', 'Poison'],
   },
 ];
