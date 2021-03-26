c586b52c48c587a110ef29287e1d28baa9bea6df
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-03-25 18:17:36 -0700

    built initial pokemonList

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-alert("Hello world");
-
-let favoriteFood = "Anything pasta related!";
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: 'Ivysaur',
+    height: 1,
+    type: ['Grass', 'Poison'],
+  },
+  {
+    name: 'Charmeleon',
+    height: 1.1,
+    type: 'Fire',
+  },
+  {
+    name: 'Ninetales',
+    height: 1.1,
+    type: 'Fire',
+  },
+  {
+    name: 'Tentacruel',
+    height: 1.6,
+    type: ['Water', 'Poison'],
+  },
+];
