af511da192fbfea1e2d8e147fcb61efa44df0dde
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-04-01 19:32:48 -0700

    script.js made IIFE compliant

--- js/script.js
+++ js/script.js
@@ -1,36 +1,63 @@
-// main pokemon list object
-let pokemonList = [
-  {
-    name: 'Ivysaur',
-    height: 1,
-    types: ['Grass', 'Poison'],
-  },
-  {
-    name: 'Charmeleon',
-    height: 1.1,
-    types: ['Fire'],
-  },
-  {
-    name: 'Ninetales',
-    height: 1.1,
-    types: ['Fire'],
-  },
-  {
-    name: 'Tentacruel',
-    height: 1.6,
-    types: ['Water', 'Poison'],
-  },
-];
+//  Creating pokemonRepository with 'add' and 'getAll' functionality
+let pokemonRepository = (function () {
+  let pokemonList = [];
 
-// forEach to iterate over pokemonList
-pokemonList.forEach(function (pokemon) {
-  //  to highlight tallest pokemon in list
-  if (pokemon.height === 1.6) {
-    document.write(
-      pokemon.name + ' (height: ' + pokemon.height + " -  Wow, that's big!<br>"
-    );
-    //  to display all other pokemon names and heights
-  } else {
-    document.write(pokemon.name + ' (height: ' + pokemon.height + ')<br>');
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+// adding pokemon to pokemonRepository
+pokemonRepository.add({
+  name: 'Ivysaur',
+  height: 1,
+  types: ['Grass', 'Poison'],
 });
+pokemonRepository.add({
+  name: 'Charmeleon',
+  height: 1.1,
+  types: ['Fire'],
+});
+pokemonRepository.add({
+  name: 'Ninetales',
+  height: 1.1,
+  types: ['Fire'],
+});
+pokemonRepository.add({
+  name: 'Tentacruel',
+  height: 1.6,
+  types: ['Water', 'Poison'],
+});
+
+// creating function to nest pokemonList inside of
+function outputListOfPokemon() {
+  // create pokemonList to be used in forEach loop
+  let pokemonList = pokemonRepository.getAll();
+
+  // forEach loop to iterate over pokemonList
+  pokemonList.forEach(function (pokemon) {
+    //  to highlight tallest pokemon in list
+    if (pokemon.height === 1.6) {
+      document.write(
+        pokemon.name +
+          ' (height: ' +
+          pokemon.height +
+          " -  Wow, that's big!<br>"
+      );
+    } else {
+      //  to display all other pokemon names and heights
+      document.write(pokemon.name + ' (height: ' + pokemon.height + ')<br>');
+    }
+  });
+}
+//  calling output function
+outputListOfPokemon();
