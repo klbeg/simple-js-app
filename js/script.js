b64f9e9892f1842e777df57faffae14ac21f3723
B33Gs <77767256+B33Gs@users.noreply.github.com>
2021-04-07 20:39:53 -0700

    script.js: cleaned up comments and reorganized functions for better readability

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 //  pokemonRepository used to create list of pokemon
-//  .add function for pushing to list
-//  .getAll function to output list of pokemon
 let pokemonRepository = (function () {
   let pokemonList = [];
-  // pushes 'pokemon' to 'pokemonList'.
-  // accessable via pokemonRepository.getAll
+  //  .add function for pushing to list
   function add(pokemon) {
     //  calls 'isValidPokemon' function to validate
     //  pending 'pokemon'
@@ -12,11 +9,11 @@ let pokemonRepository = (function () {
       //  if 'pokemon' is valid, pushes to pokemonList
       pokemonList.push(pokemon);
     } else {
-      // if 'pokemon' invaled, throws error message
+      // if 'pokemon' invalid, throws error message
       throw TypeError('Invalid object!');
     }
   }
-  // outputs full list of pokemon
+  //  .getAll function to access full list of pokemon
   function getAll() {
     return pokemonList;
   }
@@ -27,13 +24,35 @@ let pokemonRepository = (function () {
   };
 })();
 
-// checks for key values in pokemon and returns boolean
+//  outputs list of pokemon in form of '1button{name}'
+function outputListOfPokemon() {
+  let pokemonList = pokemonRepository.getAll();
+  //  iterates over pokemonList and creates: li > button{pokemon's name}
+  //  for each pokemon in list
+  pokemonList.forEach(function (pokemon) {
+    pokemonButtonCreator(pokemon);
+  });
+}
+
+//  checks for key values in pokemon and returns boolean
+//  called in pokemonRepository.add function
 function isValidPokemon(pokemon) {
   // key values expected
   return ['name', 'height', 'types'].every(function (key) {
     return pokemon.hasOwnProperty(key);
   });
 }
+
+// appends 'li' > 'button{pokemon name}' to pokemon 'ul' in index.html
+let pokemonButtonCreator = (pokemon) => {
+  let pokemonListContainer = document.querySelector('ul');
+  let listItem = document.createElement('li');
+  let button = document.createElement('button');
+  pokemonListContainer.appendChild(listItem);
+  listItem.appendChild(button);
+  button.innerText = pokemon.name;
+};
+
 //  pokemon added via pokemonRepository.add
 //  after auto validating via isValidPokemon
 pokemonRepository.add({
@@ -56,27 +75,6 @@ pokemonRepository.add({
   height: 1.6,
   types: ['Water', 'Poison'],
 });
-
-// appends 'li' > 'button{pokemon name}' to pokemon 'ul' in index.html
-let pokemonButtonCreator = (pokemon) => {
-  let pokemonListContainer = document.querySelector('ul');
-  let listItem = document.createElement('li');
-  let button = document.createElement('button');
-  pokemonListContainer.appendChild(listItem);
-  listItem.appendChild(button);
-  button.innerText = pokemon.name;
-};
-
-// outputs complete list of pokemon names as 'list items > buttons'
-// into a ul in index.html
-function outputListOfPokemon() {
-  let pokemonList = pokemonRepository.getAll();
-  //  iterates over pokemonList and creates: li > button{pokemon's name}
-  //  for each pokemon in list
-  pokemonList.forEach(function (pokemon) {
-    pokemonButtonCreator(pokemon);
-  });
-}
 
 //  calling output function
 outputListOfPokemon();
